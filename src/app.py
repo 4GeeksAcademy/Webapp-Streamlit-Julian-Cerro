c3901a885c6c7913434e38b5ce0d59094264baea
JulianCM21 <150470789+JulianCM21@users.noreply.github.com>
2024-12-20 19:32:21 +0000

    Streamlit Pjct Julian Cerro

--- src/app.py
+++ src/app.py
@@ -1,4 +1,35 @@
-from utils import db_connect
-engine = db_connect()
+import streamlit as st
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
 
-# your code here
+model_path = "/workspaces/Webapp-Streamlit-Julian-Cerro/models/knn_neighbors-5_algorithm-brute_metric-cosine.sav"
+model = pickle.load(open(model_path, "rb"))
+
+#load clean_data.csv
+data_path = "/workspaces/Webapp-Streamlit-Julian-Cerro/src/clean_data.csv"  
+data = pd.read_csv(data_path)
+
+#Train vectorizer with "tags" columns data
+vectorizer = TfidfVectorizer()
+vectorizer.fit(data["tags"])
+
+st.title("Movie recommendation")
+st.markdown("""
+This application uses a KNN model trained with TF-IDF to find similar movies based in your preferences.
+""")
+
+#Enter "tags" text
+user_input = st.text_area("Enter tags (keywords):", "example: adventure, action, science fiction")
+
+#Button to predict
+if st.button("Find recommendations"):
+    try:
+        user_vector = vectorizer.transform([user_input])
+        #Find recomm
+        distances, indices = model.kneighbors(user_vector, n_neighbors=5)
+        st.write("### Recommended movies:")
+        for i in indices[0]:
+            st.write(data.iloc[i]['title'])
+    except ValueError as e:
+        st.error("Error: make sure to imput valid data.")
